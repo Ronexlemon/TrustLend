7defd740c748ae55691143574d9e4ee6b95d39de
RonexLemon <ronexondimu@gmail.com>
2024-10-08 23:22:15 +0300

    smart wallet

--- client/trustlend/src/pages/_app.tsx
+++ client/trustlend/src/pages/_app.tsx
@@ -2,29 +2,37 @@ import "@/styles/globals.css";
 import '@coinbase/onchainkit/styles.css';
 import type { AppProps } from "next/app";
 import { OnchainKitProvider } from '@coinbase/onchainkit'; 
-import { http, createConfig } from 'wagmi'
-import { base, mainnet, optimism } from 'wagmi/chains'
+
+import { mainnet, optimism } from 'wagmi/chains'
 import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider } from 'wagmi'
+import { http, cookieStorage, createConfig, createStorage } from 'wagmi';
+import { base } from 'wagmi/chains'; 
+import { coinbaseWallet } from 'wagmi/connectors';
 
 
 
 const projectId = 'b40a827ad95a369e3b4af6fcd1904bb7'
 
  const config = createConfig({
-  chains: [mainnet, base],
+  chains: [base], 
   connectors: [
-    injected(),
-    walletConnect({ projectId }),
-    metaMask(),
-    safe(),
+    coinbaseWallet({
+      appName: "OnchainKit",
+      preference: 'smartWalletOnly',
+      version: '4',
+    }),
   ],
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+  ssr: true,
   transports: {
-    [mainnet.id]: http(),
-    [base.id]: http(),
+    [base
+.id]: http(), 
   },
-})
+});
 
 const queryClient = new QueryClient();
 
