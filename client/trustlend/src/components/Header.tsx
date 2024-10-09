f0e7a3ffc592e7c978a025e4443a9a22e7c30442
RonexLemon <ronexondimu@gmail.com>
2024-10-09 15:35:45 +0300

    landing

---
+++ client/trustlend/src/components/Header.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { Button } from "./ui/button"
+
+const Header =()=>{
+    return(
+        <div className="bg-gradient-to-r from-[#5975FF] to-[#3029D9] h-24 w-full flex justify-around items-center">
+            <div className="flex w-1/3 justify-start items-center text-white text-3xl font-bold">
+                <h1>LOGO</h1>
+                <h2 className="text-4xl">TRUSTLEND</h2>
+
+            </div>
+            <div className="flex justify-center w-1/3 gap-4 items-center ">
+                <Button variant="link" className="text-gray-300 hover:text-white ">Home</Button>
+
+                <Button variant="link" className="text-gray-300 hover:text-white ">About Us</Button>
+                <Button variant="link" className="text-gray-300 hover:text-white ">How it works</Button>
+                <Button variant="link" className="text-gray-300 hover:text-white ">Our Services</Button>
+                
+            </div>
+            <div className="flex justify-end items-end w-1/3">
+            <Button>Contact Us</Button>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
---
+++ client/trustlend/src/components/pages/Home.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Button } from "../ui/button";
+
+
+const HomeLand = ()=>{
+    return(
+        <div className="h-screen w-full ">
+            <div className="grid grid-cols-2  items-center justify-center h-3/4 w-full">
+                <div className=" flex flex-col col-span-1 gap-4 justify-center items-start">
+                    <h1 className="text-9xl  text-white">
+                        Financial
+
+                    </h1>
+                    <h1 className="text-9xl text-white">
+                        Solutions
+
+                    </h1>
+
+                    <div>
+                        <span className="text-gray-100">
+                        Inclusion in finance is more than just access—it's about enabling individuals to <br/>thrive and take  control of their economic futures.
+                        </span>
+
+                    </div>
+                    <div className="">
+                        <Button>GET START</Button>
+                        </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    )
+}
+
+export default HomeLand
